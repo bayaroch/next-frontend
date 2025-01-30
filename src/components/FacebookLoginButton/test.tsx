d0a8e6794ebdd71a655e88498c1705c9194f507a
Joy Spark <bayaroch@gmail.com>
2025-01-30 23:31:18 +0800

    fix(login): v1

--- src/components/FacebookLoginButton/test.tsx
+++ src/components/FacebookLoginButton/test.tsx
@@ -1,28 +1,44 @@
 /* eslint-disable prefer-const */
-import React, { useEffect } from 'react'
+import { FB } from '@containers/FacebookOauth'
+import React, { useEffect, useState } from 'react'
 
-const FacebookLogin = ({
+interface FacebookLoginProps {
+  appId: string
+  configId: string
+  onSuccess: (response: any) => void
+  onFailure: (error: any) => void
+  buttonText?: string
+}
+
+declare global {
+  interface Window {
+    FB: FB
+    fbAsyncInit: any
+  }
+}
+
+const FacebookLogin: React.FC<FacebookLoginProps> = ({
   appId,
   configId,
   onSuccess,
   onFailure,
-}: {
-  appId: string
-  configId: string
-  onSuccess: (v: any) => void
-  onFailure: (v: any) => void
+  buttonText = 'Login with Facebook',
 }) => {
+  const [isSdkLoaded, setIsSdkLoaded] = useState(false)
+
   useEffect(() => {
+    // Load the SDK
     ;(function (d, s, id) {
-      let js,
+      let js: HTMLScriptElement,
         fjs = d.getElementsByTagName(s)[0]
       if (d.getElementById(id)) return
-      js = d.createElement(s) as any
+      js = d.createElement(s) as HTMLScriptElement
       js.id = id
       js.src = 'https://connect.facebook.net/en_US/sdk.js'
       fjs?.parentNode?.insertBefore(js, fjs)
     })(document, 'script', 'facebook-jssdk')
 
+    // Initialize the SDK
     window.fbAsyncInit = function () {
       window.FB.init({
         appId: appId,
@@ -30,38 +46,46 @@ const FacebookLogin = ({
         xfbml: true,
         version: 'v22.0',
       })
+      setIsSdkLoaded(true)
     }
   }, [appId])
 
   const handleLogin = () => {
+    if (!isSdkLoaded) {
+      onFailure(new Error('Facebook SDK not loaded'))
+      return
+    }
+
     window.FB.login(
-      (response) => {
+      (response: any) => {
         if (response.authResponse) {
           onSuccess(response)
         } else {
           onFailure(response)
         }
       },
-      { config_id: configId, auth_type: 'rerequest', return_scopes: true }
+      {
+        config_id: configId,
+        auth_type: 'rerequest',
+        return_scopes: true,
+      }
     )
   }
 
   return (
-    <>
-      <div id="fb-root"></div>
-      <button
-        onClick={handleLogin}
-        className="fb-login-button"
-        data-width=""
-        data-size=""
-        data-button-type=""
-        data-layout=""
-        data-auto-logout-link="false"
-        data-use-continue-as="false"
-      >
-        Login with Facebook
-      </button>
-    </>
+    <button
+      onClick={handleLogin}
+      disabled={!isSdkLoaded}
+      className="fb-login-button"
+      data-width=""
+      data-size=""
+      data-button-type=""
+      data-layout=""
+      data-auto-logout-link="false"
+      data-use-continue-as="false"
+    >
+      {buttonText}
+    </button>
   )
 }
 
