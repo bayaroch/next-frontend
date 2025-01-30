e2af8e1812e59098bc839ecd565f802f9cc15788
Joy Spark <bayaroch@gmail.com>
2025-01-30 15:30:52 +0800

    fix(aa): aa

--- src/components/FacebookLoginButton/index.tsx
+++ src/components/FacebookLoginButton/index.tsx
@@ -5,6 +5,7 @@ import FacebookLogin, {
   SuccessResponse,
 } from '@greatsumini/react-facebook-login'
 import { useTranslation } from 'react-i18next'
+import { Box } from '@mui/material'
 
 interface FacebookLoginButtonProps {
   isLoading: boolean
@@ -17,37 +18,40 @@ const FacebookLoginButton: React.FC<FacebookLoginButtonProps> = ({
 }) => {
   const { t } = useTranslation()
   return (
-    <FacebookLogin
-      appId={process.env.REACT_APP_FACEBOOK_CLIENT_ID || ''}
-      fields="name,email,picture"
-      initParams={{
-        version: 'v21.0',
-        xfbml: true,
-        cookie: true,
-      }}
-      loginOptions={{
-        auth_type: 'rerequest',
-        config_id: '1179935390403786',
-      }}
-      onSuccess={onLogin}
-      scope={''}
-      // eslint-disable-next-line no-console
-      onFail={(err) => console.log('fb-error', err)}
-      render={(renderProps) => (
-        <LoadingButton
-          fullWidth
-          loading={isLoading}
-          sx={{
-            boxShadow: 1,
-          }}
-          variant="outlined"
-          onClick={renderProps.onClick}
-          startIcon={<FacebookIcon />}
-        >
-          {t('SYSCOMMON.facebook_login')}
-        </LoadingButton>
-      )}
-    />
+    <>
+      <Box>Check</Box>
+      <FacebookLogin
+        appId={process.env.REACT_APP_FACEBOOK_CLIENT_ID || ''}
+        fields="name,email,picture"
+        autoLoad={true}
+        initParams={{
+          version: 'v21.0',
+          xfbml: true,
+          cookie: true,
+        }}
+        loginOptions={{
+          auth_type: 'rerequest',
+          config_id: '1179935390403786',
+        }}
+        onSuccess={onLogin}
+        // eslint-disable-next-line no-console
+        onFail={(err) => console.log('fb-error', err)}
+        render={(renderProps) => (
+          <LoadingButton
+            fullWidth
+            loading={isLoading}
+            sx={{
+              boxShadow: 1,
+            }}
+            variant="outlined"
+            onClick={renderProps.onClick}
+            startIcon={<FacebookIcon />}
+          >
+            {t('SYSCOMMON.facebook_login')}
+          </LoadingButton>
+        )}
+      />
+    </>
   )
 }
 
